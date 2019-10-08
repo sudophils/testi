a7f9ef7d546319a4f7f0b7b075b45ed9019d48f3
phixlab <philencripted@gmail.com>
2019-10-08 21:18:45 +0100

    added some crud operation to test the mongo db in gettest action

--- api/controllers/test/gettest.js
+++ api/controllers/test/gettest.js
@@ -1,3 +1,16 @@
 module.exports = async function gettest(req, res) {
-  return res.status(200).json({ valid: 'test executed' });
+  const { title, body } = req.params();
+
+  const data = {
+    title,
+    body
+  };
+
+  if (!title) return res.status(404).json({ err: 'title is missing' });
+  if (!body) return res.status(404).json({ err: 'title is missing' });
+  const post = await Test.create({ test: data }).fetch();
+
+  if (!post) return res.status(400).json({ err: ' no post create' });
+
+  return res.status(200).json({ post: post });
 };
